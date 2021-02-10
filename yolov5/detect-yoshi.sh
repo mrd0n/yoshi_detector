ed1e32eaf02a5993df6642b72cddf0d47734022c
mrd0n <don@munroe.ca>
2021-02-10 11:54:21 -0700

    switched to using git submodules

--- yolov5/detect-yoshi.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source ~/miniconda3/etc/profile.d/conda.sh
-conda activate yolov5a
-python detect-yoshi.py --weights best-yoshi.pt --source frame.jpg --conf-thres 0.60 --project 'inference' --save-txt --save-conf --name 'output' --exist-ok --device 0
-
--- yolov5/run-rtsp-stream.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-python detect.py --weights runs/train/exp2/weights/best.pt --source rtsp://don:d344831@10.0.0.235:554/ch01/1 --device cpu
-
