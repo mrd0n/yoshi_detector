10867df2741217a23f46ea13e38830bbf53c7d7b
mrd0n <don@munroe.ca>
2021-04-29 16:39:09 -0600

    tweaking the shell start script

--- detect-yoshi.sh
+++ detect-yoshi.sh
@@ -2,6 +2,7 @@
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate yolov5a
 
+# loop if the script exits
 until python detect-yoshi.py --weights best-yoshi.pt --source frame.jpg --conf-thres 0.60 --project 'inference' --save-conf --name 'output' --exist-ok --device cpu ; do
     echo "Detection script exited with exit code $?.  Respawning.." >&2
     sleep 5
